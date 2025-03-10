08826e339fe1a60e27a43dfb78d4ceeac225f1c2
Vicky <shvicky@tuta.io>
2025-03-11 00:07:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -146,3 +146,30 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 }); 
+
+
+
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAAzWN8RBeqFfTeRahWsFlgarkh6xdPUWo",
+    authDomain: "karate-database.firebaseapp.com",
+    projectId: "karate-database",
+    storageBucket: "karate-database.firebasestorage.app",
+    messagingSenderId: "1084992955954",
+    appId: "1:1084992955954:web:7548ba8304a7abb8417e2b",
+    measurementId: "G-B8LJ49J2P1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
